34d75072b11c31373c05d68977665ef57da35544
Liam Watson <you@example.com>
2022-09-28 15:49:27 +0200

    Added health bar gradient

--- Assets/scripts/EmberHeat.cs
+++ Assets/scripts/EmberHeat.cs
@@ -5,17 +5,22 @@ using UnityEngine.UI;
 public class EmberHeat : MonoBehaviour
 {
     public Slider slider;
+    public Gradient gradient;
+    public Image fill;
     
     public void SetHealth(float health){
         slider.value = health;
+        fill.color = gradient.Evaluate(slider.normalizedValue); 
     }
 
     public void SetMaxHealth(float health){
         slider.maxValue = health;
         slider.value = health;
+        // fill.color = gradient.Evaluate(1f); 
     }
 
     public void UpdateHealth(float health){
         slider.value = slider.value + health;
+        // fill.color = gradient.Evaluate(slider.normalizedValue);
     }
 }
