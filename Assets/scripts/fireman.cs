77ec5c48d7f87a22b384f65470388911833e73d9
Sebastian Oliver <sebtheoliver@gmail.com>
2022-10-09 16:03:33 +0200

    Added same enemy ai to both woodworker and fireman.

--- Assets/scripts/fireman.cs
+++ Assets/scripts/fireman.cs
@@ -4,17 +4,44 @@ using UnityEngine;
 
 public class fireman : MonoBehaviour
 {
+    public GameObject proj;
+    public float range;
+    public float fireRate;
+
+    private GameObject ember;
+    private Vector3 spawnPos;
+    private GameObject firemanProj;
+    private float shotTime;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        ember = GameObject.Find("Ember");
+        spawnPos = new Vector3(transform.position.x - 1, transform.position.y, transform.position.z);
+        shotTime = fireRate;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float emberX = ember.transform.position.x;
+        float emberY = ember.transform.position.x;
+
+        Vector2 fireman = new Vector2(transform.position.x, transform.position.y);
+        Vector2 emb = new Vector2(emberX, emberY);
+        float dist = Vector2.Distance(emb, fireman);
+
+        if (shotTime <= 0 && dist <= range)
+        {
+            firemanProj = Instantiate(proj, spawnPos, Quaternion.identity);
+            shotTime = fireRate;
+        }
+        else 
+        {
+            shotTime -= Time.deltaTime;
+        }
     }
+
     private void OnParticleCollision(GameObject other) {
         if(other.gameObject.tag == "heatwave"){
             Destroy(gameObject);
---
+++ Assets/scripts/firemanProj.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class firemanProj : MonoBehaviour
+{
+    public float speed;
+
+    private Transform ember;
+    private Vector2 target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ember = GameObject.Find("Ember").transform;
+        target = new Vector2(ember.position.x, ember.position.y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+
+        if (transform.position.x == target.x && transform.position.y == target.y)
+        {
+            DestroyProj();
+        }
+    }
+    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            DestroyProj();
+        }
+    }
+
+    void DestroyProj()
+    {
+        Destroy(gameObject);
+    }
+}
