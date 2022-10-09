f72491a3c5fc48636560d7083a08c4cd16daebc1
Sebastian Oliver <sebtheoliver@gmail.com>
2022-10-09 15:15:58 +0200

    Grasslander now shoots towards ember, grassblade is destroyed when it gets to the position where ember was.

---
+++ Assets/scripts/grassBlade.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class grassBlade : MonoBehaviour
+{
+    public float speed;
+
+    private Transform ember;
+    private Vector2 target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ember = GameObject.Find("Ember").transform;
+        target = new Vector2(ember.position.x, ember.position.y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+
+        if (transform.position.x == target.x && transform.position.y == target.y)
+        {
+            DestroyBlade();
+        }
+    }
+    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            DestroyBlade();
+        }
+    }
+
+    void DestroyBlade()
+    {
+        Destroy(gameObject);
+    }
+}
--- Assets/scripts/grasslander.cs
+++ Assets/scripts/grasslander.cs
@@ -4,16 +4,42 @@ using UnityEngine;
 
 public class grasslander : MonoBehaviour
 {
+    public GameObject blade;
+    public float range;
+    public float fireRate;
+
+    private GameObject ember;
+    private Vector3 currentPos;
+    private GameObject grassBlade;
+    private float shotTime;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        ember = GameObject.Find("Ember");
+        currentPos = transform.position;
+        shotTime = fireRate;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float emberX = ember.transform.position.x;
+        float emberY = ember.transform.position.x;
+
+        Vector2 grass = new Vector2(currentPos.x, currentPos.y);
+        Vector2 emb = new Vector2(emberX, emberY);
+        float dist = Vector2.Distance(emb, grass);
+
+        if (shotTime <= 0 && dist <= range)
+        {
+            grassBlade = Instantiate(blade, currentPos, Quaternion.identity);
+            shotTime = fireRate;
+        }
+        else 
+        {
+            shotTime -= Time.deltaTime;
+        }
     }
 
     private void OnParticleCollision(GameObject other) {
